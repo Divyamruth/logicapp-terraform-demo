3723b930665d013da81d2ba2589f59a96997befe
divyamruth <amruthakavi.divyasree@capgemini.com>
2026-05-20 12:28:52 +0530

    Initial Terraform Logic App setup with ARM template

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_resource_group" "rg" {
+  name = "my-first-rg-demo"
+}
+
+resource "azurerm_resource_group_template_deployment" "logicapp" {
+  name                = "logicapp-arm-deploy"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  deployment_mode     = "Incremental"
+
+  template_content = file("workflow.json")
+}
